9f5bcad1c72c705462f470fc8ca1d6dad3d359f5
fademebets <brennanquan3@gmail.com>
2025-05-31 05:36:16 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,6 +27,8 @@ app.use(express.json());
 app.use(cors({
   origin: ['https://fademebets.com', 'https://www.fademebets.com', 'http://127.0.0.1:5500'],
   credentials: true,
+    optionsSuccessStatus: 200
+
 }));
 
 
