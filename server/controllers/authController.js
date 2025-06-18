e43fdd0a97bbe285a53281eb216045cb32761364
Talha Chaudhary <chaudharytalhaofficial@gmail.com>
2025-06-18 13:05:48 +0500

    fix token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,7 +28,7 @@ exports.login = async (req, res) => {
   if (!isMatch) return res.status(401).json({ error: 'Invalid credentials' });
 
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '1d',
+    expiresIn: '30d',
   });
 
   res.json({
