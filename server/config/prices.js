dc006c0a49bd64d0ed3aa67bb23aafe2e3535e99
fademebets <brennanquan3@gmail.com>
2025-06-20 19:15:28 +0500

    Update prices.js

--- server/config/prices.js
+++ server/config/prices.js
@@ -2,5 +2,5 @@
 module.exports = {
   monthly: 'price_1RbHXtLfWCnW3JgT8k6UbFOv',
   quarterly: 'price_1RbHYnLfWCnW3JgT06wzhlLV',
-  yearly: 'price_1RbHZSLfWCnW3JgTqGo1LsHe',
+  yearly: 'price_1Rc5hmLfWCnW3JgTz47u7nXi',
 };
